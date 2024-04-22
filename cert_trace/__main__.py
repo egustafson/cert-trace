5611c6fde108227e237aef7a752c1cfb9290b2ab
Andy Ellis <andy.ellis@viasat.com>
2024-04-22 04:25:22 +0000

    Print cert date validity

--- cert_trace/__main__.py
+++ cert_trace/__main__.py
@@ -60,13 +60,27 @@ class Cert:
     def set_auth_index(self, idx):
         self.auth_index = idx
 
+    def date_is_valid(self):
+        now = datetime.now()
+        if (self.cert.not_valid_after >= now) and (self.cert.not_valid_before) <= now:
+            return True
+        return False
+
+    def show_date_validity(self):
+        if self.date_is_valid():
+            return("VALID:  ")
+        return("INVALID:")
+
     def __str__(self):
         out = io.StringIO()
         print("------", file=out)
         print("{:>5}: Subject:  {}".format(self.index, self.subject()), file=out)
         print("       Subject Key Identifier:        {}".format(self.subjectKeyId()), file=out)
         print("       Issuer:   {}".format(self.issuer()), file=out)
-        print("       Valid:    {} <-> {}".format(self.cert.not_valid_before_utc, self.cert.not_valid_after_utc), file=out)
+        print("       {}  {} <-> {}".format(self.show_date_validity(),
+                                               self.cert.not_valid_before_utc,
+                                               self.cert.not_valid_after_utc),
+                                            file=out)
         authKeyId = self.authorityKeyId()
         if authKeyId:
             if len(self.auth_index) > 0:
